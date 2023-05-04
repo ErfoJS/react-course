98a9c306c11819046979944c1dea206659e078e8
Michał Gawroński <mic.gawronski@gmail.com>
2023-05-04 12:58:47 +0200

    Added ExpensesList.js and ExpensesList.css. Conditional logic inside.

--- first-course-app/src/components/Expenses/ExpensesCombine.js
+++ first-course-app/src/components/Expenses/ExpensesCombine.js
@@ -3,38 +3,32 @@ import "./Styles/ExpensesCombine.css";
 import Card from "../UI/Card";
 import Filter from "./ExpensesFilter.js";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList.js";
 
 function ExpensesCombine(props) {
   const [inputedYear, setYear] = useState("2020");
 
   const FilterDataHandler = (inputedYear) => {
     setYear(inputedYear);
-    console.log(inputedYear);
+    //console.log(inputedYear);
   };
 
   const filterdExpenses = props.items.filter(
     (expense) => expense.date.getFullYear() === +inputedYear
   );
 
-  let filterLogic = <p>No expenses found.</p>;
-  if (filterdExpenses.length > 0) {
-    filterLogic = filterdExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
-    <div>
-      <Card className="expenses">
-        <Filter yearDefault={inputedYear} onFilterHandler={FilterDataHandler} />
-        {filterLogic}
-      </Card>
-    </div>
+    <li>
+      <div>
+        <Card className="expenses">
+          <Filter
+            yearDefault={inputedYear}
+            onFilterHandler={FilterDataHandler}
+          />
+          <ExpensesList items={filterdExpenses} />
+        </Card>
+      </div>
+    </li>
   );
 }
 export default ExpensesCombine;
