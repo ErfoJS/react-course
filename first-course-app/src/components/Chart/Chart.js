7f0d9599d182f04b25995eea00c26dae8f2cf673
Michał Gawroński <mic.gawronski@gmail.com>
2023-05-06 13:32:43 +0200

    Chart logic prepare

---
+++ first-course-app/src/components/Chart/Chart.js
@@ -0,0 +1,19 @@
+import ChartBar from "./ChartBar.js";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+          key={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Chart;
