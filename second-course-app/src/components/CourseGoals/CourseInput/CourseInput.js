3f2c3876d90e9bd0eedc3f7456ddf60490b9c262
Michał Gawroński <mic.gawronski@gmail.com>
2023-05-07 19:31:23 +0200

    styled-components use

--- second-course-app/src/components/CourseGoals/CourseInput/CourseInput.js
+++ second-course-app/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+
+const FormControl = styled.div`
+  & {
+    margin: 0.5rem 0;
+  }
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: #e66e6e;
+    border-color: red;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -9,6 +45,9 @@ const CourseInput = (props) => {
 
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
   };
 
   const formSubmitHandler = (event) => {
@@ -22,17 +61,10 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
-        <input
-          style={{
-            background: !isValid ? "salmon" : "transparent",
-            borderColor: !isValid ? "black" : "black",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-      </div>
+      <FormControl className={!isValid && " invalid"}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
