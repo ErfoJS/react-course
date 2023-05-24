b84e9c0075905426a24ca2a70b42e22ac2f7eff9
Michał Gawroński <mic.gawronski@gmail.com>
2023-05-24 14:03:24 +0200

    Error modified (portaled up in HTML)

--- add-user-app/src/Components/UI/Error.js
+++ add-user-app/src/Components/UI/Error.js
@@ -1,24 +1,46 @@
 import styles from "./Styles/Error.module.css";
 import Button from "./Button";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onRemoveError}></div>;
+};
+
+const ErrorOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+
+      <footer className={styles.actions}>
+        <div className={styles.errorButton}>
+          <Button onClick={props.onRemoveError}>Okay</Button>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
 const Error = (props) => {
   return (
-    <div className={styles.backdrop} onClick={props.onRemoveError}>
-      <div></div>
-      <div className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-
-        <footer className={styles.actions}>
-          <div className={styles.errorButton}>
-            <Button onClick={props.onRemoveError}>Okay</Button>
-          </div>
-        </footer>
-      </div>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onRemoveError={props.onRemoveError} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ErrorOverlay
+          title={props.title}
+          message={props.message}
+          onRemoveError={props.onRemoveError}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </>
   );
 };
 
